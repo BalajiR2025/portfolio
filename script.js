f1e8a69ec0a69143d1189c326b092eb1aff01ec2
BalajiR2025 <balajirameshofficial@gmail.com>
2025-11-05 09:27:07 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Portfolio Loaded");
+const elements = document.querySelectorAll('.fade-in');
+function showOnScroll() {
+  elements.forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 50) {
+      el.classList.add('visible');
+    }
+  });
+}
+window.addEventListener('scroll', showOnScroll);
+showOnScroll();
